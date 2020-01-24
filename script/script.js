a255c7a5b965d6fbff71066b4c5ee097bc4fae60
Vitaly <vital.sulimov@yandex.ru>
2020-01-24 20:07:15 +0300

    Lesson 2

--- script/script.js
+++ script/script.js
@@ -1,8 +1,19 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
-alert('Я стану программистом?');
-console.log(" очень на это надеюсь")
+let money = 10000;
+let income = 'Фриланс';
+let addExpenses = 'Интернет, комуналка, такси, автобус, кино';
+let deposit = true;
+let mission = 100000;
+let period = 6;
+// alert('Я стану программистом?');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof addExpenses);
+console.log(addExpenses.length);
+console.log('Период равен' + ' ' + period + ' ' + 'месяцев.' + ' ' + 'Цель заработать' + ' ' + mission + ' ' + 'долларов.' + ' ');
+
+console.log(addExpenses.toLocaleLowerCase());
+console.log(addExpenses.substring());
+
+
+let budgetDay = money / 30;
+console.log(budgetDay);
