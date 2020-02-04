6397a10689d15b92f673817742ef2f7a62da4eb1
vsulimovvv <60267032+vsulimovvv@users.noreply.github.com>
2020-02-04 21:17:00 +0300

    show my code

--- script/script.js
+++ script/script.js
@@ -8,7 +8,7 @@ let money,
 
   start = function () {
     do {
-      money = prompt('Ваш месячный доход?');
+      money = prompt('Ваш месячный доход?', 50000);
     }
     while (!isNumber(money));
   };
@@ -27,15 +27,35 @@ let appData = {
   budgetDay: 0,
   budgetMonth: 0,
   expensesMonth: 0,
+  persentDeposit: 0,
+  moneyDeposit: 0,
   asking: function () {
+    if (confirm('Есть ли у вас дополнительный источник заработка?')) {
+      let itemIncome;
+      do {
+        itemIncome = prompt('Какой у вас дополнительный заработок?', 'Таксую');
+      } while (isNumber(itemIncome));
+      let cashIncome;
+      do {
+        cashIncome = prompt('Сколько в месяц вы на этом зарабатываете?', 10000);
+      } while (!isNumber(cashIncome));
+      appData.income[itemIncome] = cashIncome;
+    }
+
     let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-    appData.addExpenses = addExpenses.toLocaleLowerCase().split(', ');
+    // appData.addExpenses = addExpenses.toLocaleLowerCase().split(', ');
+    appData.addExpenses = addExpenses.split(/\s+/).map(word => word[0].toUpperCase() + word.substring(1)).join(' ');
+
     appData.deposit = confirm('Есть ли у вас депозит в банке?');
     for (let i = 0; i < 2; i++) {
-      let expenses1 = prompt('Введите обязательную статью расходов?');
+      let expenses1;
+      do {
+        expenses1 = prompt('Введите обязательную статью расходов?', 'квартира');
+      }
+      while (isNumber(expenses1));
       let expenses2;
       do {
-        expenses2 = prompt('Во сколько это обойдется?');
+        expenses2 = prompt('Во сколько это обойдется?', 10000);
       }
       while (!isNumber(expenses2));
       appData.expenses[expenses1] = +expenses2;
@@ -63,16 +83,74 @@ let appData = {
     } else if (appData.budgetDay < 0) {
       return ('Что то пошло не так');
     }
+  },
+
+  getInfoDeposit: function () {
+    if (appData.deposit) {
+      do {
+        appData.persentDeposit = prompt('Какой годовой процент?', '10');
+      } while (!isNumber(appData.persentDeposit));
+      do {
+        appData.moneyDeposit = prompt('Какая сумма заложена?', 10000);
+      }
+      while (!isNumber(appData.moneyDeposit));
+    }
+  },
+  calcSavedMoney: function () {
+    return (appData.budgetMonth * appData.period);
   }
 };
 
 appData.asking();
 appData.getExpensesMonth();
+
 console.log('Расходы за месяц: ' + appData.expensesMonth);
 appData.getBudget();
 console.log('Период достижения цели: ' + Math.ceil(appData.getTargetMonth()));
 console.log(appData.getStatusIncome());
+console.log(appData.persentDeposit, appData.moneyDeposit);
+appData.getInfoDeposit();
 
 for (let key in appData) {
   console.log(key + ': ', appData[key]);
 }
+
+
+
+// console.log(appData.calcSavedMoney, appData.moneyDeposit, appData.calcSavedMoney());
+
+// for (let i = 0; i < 2; i++) {
+// let expenses1 = prompt('Введите обязательную статью расходов?', 'квартира');
+//   let expenses1;
+//   let expenses2;
+//   do {
+//     expenses1 = prompt('Введите обязательную статью расходов?', 'квартира');
+//     expenses2 = prompt('Во сколько это обойдется?', 3000);
+
+//   }
+//   while (!isNumber(expenses2) && !isNumber(expenses1));
+//   appData.expenses[expenses1] = +expenses2;
+// }
+
+
+// appData.persentDeposit = prompt('Какой годовой процент?', '10');
+//     appData.moneyDeposit = prompt('Какая сумма заложена?', 10000);
+// let expenses1;
+//     do {
+//       expenses1 = prompt('Введите обязательную статью расходов?', 'квартира');
+//     }
+//     while (isNumber(expenses1));
+//     let expenses2;
+//     do {
+//       expenses2 = prompt('Во сколько это обойдется?', 3000);
+//     }
+//     while (!isNumber(expenses2));
+//     appData.expenses[expenses1] = +expenses2;
+
+// if (appData.deposit) {
+//   appData.persentDeposit = prompt('Какой годовой процент?', '10');
+//   do {
+//     appData.moneyDeposit = prompt('Какая сумма заложена?', 10000);
+//   }
+//   while (!isNumber(appData.moneyDeposit));
+// }
